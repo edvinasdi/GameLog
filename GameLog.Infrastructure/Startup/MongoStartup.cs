8e48b472fb17cff4a8da19cb5ad9a913b96f0889
Edvin <edvinas.dichkanec@gmail.com>
2025-03-01 22:16:18 +0200

    Added Infrastructure layer

---
+++ GameLog.Infrastructure/Startup/MongoStartup.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace GameLog.Infrastructure.Startup;
+
+public static class MongoStartup
+{
+    public static void RegisterDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var mongoDbSection = configuration.GetSection("MongoDb");
+
+        var mongoClientSettings = MongoClientSettings.FromConnectionString(mongoDbSection["ConnectionString"]);
+        var mongoClient = new MongoClient(mongoClientSettings);
+
+        services
+            .AddSingleton<IMongoClient>(mongoClient)
+            .AddSingleton<IMongoDatabase>(sp =>
+            {
+                var gameLogDatabaseName = mongoDbSection["GameLogDatabase"];
+                return sp
+                    .GetRequiredService<MongoClient>()
+                    .GetDatabase(gameLogDatabaseName);
+            });
+        
+        RegisterCollections(services);
+    }
+
+    public static void RegisterCollections(this IServiceCollection services)
+    {
+        
+    }
+}
